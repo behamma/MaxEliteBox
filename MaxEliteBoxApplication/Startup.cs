490afe12dc51d6569a01c4683fe67e7a777d7be2
behamma <alierenilhann@gmail.com>
2023-11-05 03:11:11 +0300

    Bir kontrol edelim

---
+++ MaxEliteBoxApplication/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaxEliteBoxApplication
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = "Host=localhost;Database=MaxEliteBox;Username=postgres;Password=myelitebox";
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(connectionString));
+        }
+    }
+}
