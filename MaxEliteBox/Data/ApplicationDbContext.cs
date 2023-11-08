cac549ed3fc53958e0307cc7438507b48fff52c9
behamma <alierenilhann@gmail.com>
2023-11-08 23:48:09 +0300

    test

---
+++ MaxEliteBox/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MaxEliteBoxApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaxEliteBoxApplication
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<MeetingPerson> MeetingsPersons { get; set; }
+        public DbSet<MeetingResult> MeetingResults { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Database=MaxEliteBox;Username=postgres;Password=alieren");
+            }
+        }
+    }
+}
